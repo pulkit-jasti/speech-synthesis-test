525db588e6735b26ad4dab67b07c7d30511f7e97
Pulkit Jasti <pulkit.jasti@gmail.com>
2021-06-09 07:18:00 +0530

    Voice function added and styling changes

--- script.js
+++ script.js
@@ -1,56 +1,70 @@
-if ('speechSynthesis' in window) {
-	var msg = new SpeechSynthesisUtterance();
+window.addEventListener('DOMContentLoaded', event => {
+	if ('speechSynthesis' in window) {
+		var msg = new SpeechSynthesisUtterance();
 
-	let volume = 0;
-	let rate = 0;
-	let pitch = 0;
+		let volume = 0;
+		let rate = 0;
+		let pitch = 0;
+		let voiceIndex = 0;
 
-	document.getElementById('volume-input').addEventListener('change', function () {
-		document.getElementById('volume-value').innerHTML = this.value;
-		volume = this.value;
-	});
+		let voicesList = document.getElementById('voice-list');
 
-	document.getElementById('rate-input').addEventListener('change', function () {
-		document.getElementById('rate-value').innerHTML = this.value;
-		rate = this.value;
-	});
+		let s = setSpeech();
+		s.then(voices => {
+			console.log(voices);
+			voices.forEach((element, index) => {
+				voicesList.innerHTML += `<option value="${index}">${element.name}</option>`;
+			});
+		});
 
-	document.getElementById('pitch-input').addEventListener('change', function () {
-		document.getElementById('pitch-value').innerHTML = this.value;
-		pitch = this.value;
-	});
+		voicesList.addEventListener('change', () => {
+			voiceIndex = voicesList.value;
+		});
 
-	document.getElementById('speak').addEventListener('click', () => {
-		msg.text = document.getElementById('main-text').value;
+		document.getElementById('volume-input').addEventListener('change', function () {
+			document.getElementById('volume-value').innerHTML = this.value;
+			volume = this.value;
+		});
 
-		var voices = window.speechSynthesis.getVoices();
-		msg.voice = voices[2];
+		document.getElementById('rate-input').addEventListener('change', function () {
+			document.getElementById('rate-value').innerHTML = this.value;
+			rate = this.value;
+		});
 
-		// Settings
-		msg.volume = volume;
-		msg.rate = rate;
-		msg.pitch = pitch;
+		document.getElementById('pitch-input').addEventListener('change', function () {
+			document.getElementById('pitch-value').innerHTML = this.value;
+			pitch = this.value;
+		});
 
-		console.log(msg);
-		speechSynthesis.speak(msg);
-	});
-} else {
-	alert("Sorry, your browser doesn't support the speech synthesis API !");
-}
+		document.getElementById('speak').addEventListener('click', () => {
+			msg.text = document.getElementById('main-text').value;
 
-function setSpeech() {
-	return new Promise(function (resolve, reject) {
-		let synth = window.speechSynthesis;
-		let id;
+			var voices = window.speechSynthesis.getVoices();
+			msg.voice = voices[voiceIndex];
 
-		id = setInterval(() => {
-			if (synth.getVoices().length !== 0) {
-				resolve(synth.getVoices());
-				clearInterval(id);
-			}
-		}, 10);
-	});
-}
+			// Settings
+			msg.volume = volume;
+			msg.rate = rate;
+			msg.pitch = pitch;
 
-let s = setSpeech();
-s.then(voices => console.log(voices));
+			console.log(msg);
+			speechSynthesis.speak(msg);
+		});
+	} else {
+		alert("Sorry, your browser doesn't support the speech synthesis API !");
+	}
+
+	function setSpeech() {
+		return new Promise(function (resolve, reject) {
+			let synth = window.speechSynthesis;
+			let id;
+
+			id = setInterval(() => {
+				if (synth.getVoices().length !== 0) {
+					resolve(synth.getVoices());
+					clearInterval(id);
+				}
+			}, 10);
+		});
+	}
+});
